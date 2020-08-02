955d24085e3b1c0396ac21eab2f83af9f26e6fba
Baris Karaer <bariskaraerx@gmail.com>
2020-08-02 14:33:53 +0300

    Unit Of Work Interface

---
+++ UdemyProject.Core/UnitOfWorks/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyProject.Core.Repositories;
+
+namespace UdemyProject.Core.UnitOfWorks
+{
+    interface IUnitOfWork
+    {
+        IProductRepository Products { get;  }
+        ICategoryRespository Categories { get; }
+        Task CommitAsync();
+        void Commit();
+    }
+}
