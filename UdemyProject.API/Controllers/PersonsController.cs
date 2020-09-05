f07ac6aa17812caee992f10d2a30943680ef7e4b
Baris Karaer <bariskaraerx@gmail.com>
2020-09-05 14:31:29 +0300

    Person controller demo

---
+++ UdemyProject.API/Controllers/PersonsController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using UdemyProject.Core.Models;
+using UdemyProject.Core.Services;
+
+namespace UdemyProject.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonsController : ControllerBase
+    {
+        private readonly IService<Person> _personService;
+
+        public PersonsController(IService<Person> service)
+        {
+            _personService = service;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var persons = await _personService.GetAllAsync();
+            return Ok(persons);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Save(Person person)
+        {
+            // normally we should use dto s but i will be doing that later
+            var _person = await _personService.AddAsync(person);
+            return Ok(_person);
+        }
+
+    }
+}
