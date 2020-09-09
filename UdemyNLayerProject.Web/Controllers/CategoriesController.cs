f35fec696b3c19791162bb63580f46264fe55698
Baris Karaer <bariskaraerx@gmail.com>
2020-09-09 13:47:24 +0300

    Create page creation

--- UdemyNLayerProject.Web/Controllers/CategoriesController.cs
+++ UdemyNLayerProject.Web/Controllers/CategoriesController.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using UdemyProject.Core.Models;
 using UdemyProject.Core.Services;
+using UdemyProject.Web.DTOs;
 
 namespace UdemyNLayerProject.Web.Controllers
 {
@@ -19,9 +21,22 @@ namespace UdemyNLayerProject.Web.Controllers
             _categoryService = categoryService;
             _mapper = mapper;
         }
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
+        {
+            var categories = await _categoryService.GetAllAsync();
+            return View(_mapper.Map<IEnumerable<CategoryDto>>(categories));
+        }
+
+        public IActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CategoryDto categoryDto)
+        {
+            await _categoryService.AddAsync(_mapper.Map<Category>(categoryDto));
+            return RedirectToAction("Index");
         }
     }
 }
